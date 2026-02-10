577140679c6a4995364b625d99a9c8b8340c53a7
Remilekun Salami <remilekun.salami@gmail.com>
2026-02-10 11:35:32 +0100

    refactor: remove unused QR code and URL shortening functionalities

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { getHome } from "./controller/home.controller";
 import { getGeo } from "./controller/geo.controller";
-import { generateQR } from "./controller/qr.controller";
-import {
-  shortenUrl,
-  getAnalytics,
-  redirectUrl,
-} from "./controller/url.controller";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -21,10 +15,6 @@ app.use(cookieParser());
 
 app.get("/", getHome);
 app.get("/api/geo", getGeo);
-app.get("/api/qr/generate", generateQR);
-app.post("/api/shorten", shortenUrl);
-app.get("/api/analytics/:shortCode", getAnalytics);
-app.get("/:shortCode", redirectUrl);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
